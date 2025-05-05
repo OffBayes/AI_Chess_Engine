80ccef1bc5078d42354d2acaf119907a92b41b6b
OffBayes <CWM.DataScience@gmail.com>
2025-05-04 22:05:45 -0400

    Fixed space function. Added pawn island function

--- AI_Engine_Functions.py
+++ AI_Engine_Functions.py
@@ -8,6 +8,8 @@ Purpose: Create a Chessbot
 """
 # Imports
 import chess
+import re
+
 white = True
 black = False
 
@@ -39,34 +41,61 @@ def current_space(board):
     board: the current board space.
     
     It returns:
-    A tuple where ther first position is the white space score and the second
-    is the black space score.
+    An integer which indicates how much space the current player controls.
     """
     hyp_board = board.copy()
     
     # Creates a list of strings containing all the current legal moves
-    hyp_board.turn = white
-    white_legal_moves = ''.join([i.uci() for i in hyp_board.legal_moves])
+    legal_moves = ''.join([i.uci() for i in hyp_board.generate_legal_moves()])
     result=''
     # Removes the starting squares so that only the end squares remain
-    for i in range(0, len(white_legal_moves), 4):
-        result += white_legal_moves[i:i+2]  # take 2 characters, skip 2
+    for i in range(2, len(legal_moves), 4):
+        result += legal_moves[i:i+2]  # take 2 characters, skip 2
     
-    white_score = legal_moves.count()
+    """
+    Checks which color the current player is. This is necessary to know what 
+    rows are owned by the oppononent.
+    """
+    if hyp_board.turn == white:
+        opponent_rows = ["5","6","7","8"]
+    else:
+        opponent_rows = ["1","2","3","4"]
+    score = sum(result.count(moves) for moves in opponent_rows)
+    return score
     
-    hyp_board.turn = black
-    black_legal_moves = ''.join([i.uci() for i in hyp_board.legal_moves])
-    black_score = legal_moves.count()
+def current_pawnchain(board):
+    """
+    This function calculates the current number of pawn chains. 
+    
+    It takes one argument:
+    board: the current board space.
+    
+    It returns:
+    An integer which indicates how many pawn chains the current player has.
+    """
+    hyp_board = board.copy()
+    
+    # Checks the color of the current player
+    if hyp_board.turn == white:
+        C = 'P'
+    else:
+        C = 'p'
+    
+    # Numerical notation of pawn squares
+    pawn_squares_num = [square for square, piece in hyp_board.piece_map() if piece == chess.Piece.from_symbol(C)]
+    
+    # String notation of pawn squares
+    pawn_loc = ''.join([chess.square_name(i) for i in pawn_squares_num])
 
-    
-def score_pos(board, depth):
+def score_pos(board, depth, weights=[1,1,1]):
     """
     This function scores a position. It scores based on 4 categories — material,
     development, squares controlled, king safety, etc.
     
-    It takes two arguments:
-    board: the current board space.
-    depth: how many moves ahead should be calculated.
+    It takes three arguments:
+    board: The current board space.
+    depth: How many moves ahead should be calculated.
+    weights:  
     
     It returns:
     A numerical score of the position, where 0 means it is equal, a negative
