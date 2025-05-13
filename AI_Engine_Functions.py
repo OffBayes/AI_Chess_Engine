9c5967fbcdfdcf139d769a690bed460cfb3cff0f
OffBayes <CWM.DataScience@gmail.com>
2025-05-12 21:07:54 -0400

    Finished minimax function for 1-ply.

--- AI_Engine_Functions.py
+++ AI_Engine_Functions.py
@@ -10,15 +10,16 @@ Purpose: Create a Chessbot
 import chess
 import re
 
-white = True
-black = False
-
 class ChessEngine:
     """
     This class provides a base set of functions that may apply to all chess 
     engines.
     
     """
+    
+    white = True
+    black = False
+    
     @staticmethod
     def legal_moves_list(board):
         hyp_board = board.copy()
@@ -41,45 +42,49 @@ class ChessEngine:
         A dictionary of the top five move choices and their associated scores.
         """        
         hyp_board = board.copy()
-        legal_moves= legal_moves_list(hyp_board)
+        legal_moves= ChessEngine().legal_moves_list(hyp_board)    
         
+    def minimax(board_node, eval_func):
+        """
+        This function finds the maximum value move for one full turn cycle.
+        It assumes the opponent plays optimally (according to the evaluation
+        function. It returns the maximum value move and the value for the
+        current player.
         
-        def minimax(board_node, eval_func):
-            """
-            This function finds the maximum value move for one full turn cycle.
-            It assumes the opponent plays optimally (according to the evaluation
-            function. It returns the maximum value move and the value for the
-            current player.
+        It takes four arguments:
+        board_node: a hypothetical board state that could be reached..
+        
+        It returns:
+        A tuple where the best move is first and the score is second.
+        """    
+        hyp_board = board_node.copy()
+        legal_moves= ChessEngine().legal_moves_list(board_node)
+        
+        scores = []
+        
+        # This layer of for loop scores all of the current player's moves - MAX
+        for move in legal_moves:
+            hyp_board.push_san(move)
+            opp_legal_moves = ChessEngine().legal_moves_list(hyp_board)
             
-            It takes four arguments:
-            board_node: a hypothetical board state that could be reached..
+            # This is a list of the scores after the opponent makes each move
+            response_scores = []
             
-            It returns:
-            A tuple where the best move is first and the score is second.
-            """    
-            hyp_board = board_node.copy()
-            legal_moves= legal_moves_list(board_node)
+            # This layer of for loop scores all of the opponent's moves - MIN
+            for opp_move in opp_legal_moves:
+                hyp_board.push_san(opp_move)
+                response_scores.append(eval_func(hyp_board))
+                hyp_board.pop()
+            # We assume the opponent makes the best move, which is the move the minimizes the score
+            scores.append(min(response_scores))
+            hyp_board.pop()
             
-            scores = []
-            
-            # This layer of for loop scores all of the current player's moves - MAX
-            for move in legal_moves:
-                hyp_board.push_san(move)
-                opp_legal_moves = legal_moves_list(hyp_board)
-                
-                # This is a list of the scores after the opponent makes each move
-                response_scores = []
-                
-                # This layer of for loop scores all of the opponent's moves - MIN
-                for opp_move in opp_legal_moves:
-                    hyp_board.push_san(opp_move)
-                    response_scores.append(eval_func(hyp_board))
-                    hyp_board = board_node.copy().push_san(move)
-                # We assume the opponent makes the best move, which is the move the minimizes the score
-                scores.append(min(response_scores))
-                hyp_board = board_node.copy()
+        # Return the best move and associated maximum score value
+        best_move = (legal_moves[scores.index(max(scores))], max(scores))
+
+        return best_move
                     
-    
+    @staticmethod
     def current_material(board):
         """
         This function calculates the current material score. It uses common scoring
@@ -130,12 +135,29 @@ class ChessEngine:
         Checks which color the current player is. This is necessary to know what 
         rows are owned by the oppononent.
         """
-        if hyp_board.turn == white:
+        if hyp_board.turn == ChessEngine().white:
             opponent_rows = ["5","6","7","8"]
         else:
             opponent_rows = ["1","2","3","4"]
         score = sum(result.count(moves) for moves in opponent_rows)
         return score
+    
+    def current_development(board):
+        """
+        This function calculates the current piece development. It scores 
+        development based on whether the current square is the same as the starting
+        square.
+        
+        It takes one argument:
+        board: the current board space.
+        
+        It returns:
+        An integer which shows how much more developed the current player is
+        compared to the opponent.
+        """
+        hyp_board = board.copy()
+        
+        
         
     def current_pawnchain(board):
         """
@@ -150,7 +172,7 @@ class ChessEngine:
         hyp_board = board.copy()
         
         # Checks the color of the current player
-        if hyp_board.turn == white:
+        if hyp_board.turn == ChessEngine().white:
             C = 'P'
         else:
             C = 'p'
@@ -164,22 +186,19 @@ class ChessEngine:
         pawn_loc = ''.join(sorted(pawn_loc))     # Sorts the columns alphabetically
         
     
-def score_pos(board, depth, weights=[1,1,1]):
-    """
-    This function scores a position. It scores based on 4 categories — material,
-    development, squares controlled, king safety, etc.
+    def score_pos(board, weights=[1, 0.5]):
+        """
+        This function scores a position. It scores based on 4 categories — material,
+        development, squares controlled, king safety, etc.
+        
+        It takes two arguments:
+        board: The current board space.
+        weights: The relative value of different points
+        
+        It returns:
+        A numerical score of the position, where 0 means it is equal, a negative
+        means it favors black, and a positive means it favors white.
+        """
+        w,b = ChessEngine().current_material(board)
+        return weights[0]*(w-b)
     
-    It takes three arguments:
-    board: The current board space.
-    depth: How many moves ahead should be calculated.
-    weights:  
-    
-    It returns:
-    A numerical score of the position, where 0 means it is equal, a negative
-    means it favors black, and a positive means it favors white.
-    """
-    
-    # Makes a copy of the board so the function does not change the true board.
-    hyp_board = board.copy()
-    # Creates a list of strings containing all the current legal moves
-    legal_moves = [i.uci() for i in hyp_board.legal_moves]
