e12dfe3d43b06d87d528545fc625bfa7d2091345
OffBayes <CWM.DataScience@gmail.com>
2025-06-04 04:07:00 -0400

    Restructured code to be more object-oriented. Added EvalEngine and SearchEngine classes, which compose ChessEngine.

--- AI_Engine_Functions.py
+++ AI_Engine_Functions.py
@@ -11,57 +11,93 @@ import chess
 import re
 import numpy as np
 
+# Functions
+def legal_moves_list(board):
+    hyp_board = board.copy()
+    legal_moves = [i.uci() for i in hyp_board.legal_moves]
+    return legal_moves  
+
+# Classes
 class ChessEngine:
     """
-    This class provides a base set of functions that may apply to all chess 
-    engines.
-    
+    A customizable chess engine composed of an evaluation engine, search engine,
+    and general evaluation functions.
+
+    This class allows users to configure the engine's behavior by supplying
+    different components at initialization time.
     """
     
-    white = True
-    black = False
-    
-    @staticmethod
-    def legal_moves_list(board):
-        hyp_board = board.copy()
-        legal_moves = [i.uci() for i in hyp_board.legal_moves]
-        return legal_moves
-   
-    
-    #def find_move(board, eval_func, depth=4, deep_search='None'):
+    def __init__(self, search, evaluation):
         """
-        This function calculates the best move based on the position evaluation
-        function. Any position evaluation function can be passed to it. Find_move
-        assumes the current player is the player to optimize winning chances for.
+        Initializes the engine with specific components.
+    
+        Args:
+            search: The engine's search component.
+            evaluation: The engine's board evaluation component.
+        """
+        self.searchEng = search
+        self.evalEng = evaluation
+        self.white = True
+        self.black = False
+                    
+    def evaluate(self, board):
+        """Returns the evaluation score of the given board."""
+        return self.evalEng.score_pos(board)
+
+    def find_best_move(self, board, depth=4):
+        """Uses the composed search engine to find the best move."""
+        return self.searchEng.search(board, self.evaluate, depth)
+    
+    
+class SearchEng:
+    """
+    A customizable search engine that allows different 
+
+    This class allows users to configure the engine's behavior by supplying
+    different components at initialization time.
+
+    """    
+    
+    
+    def minimax(self, board_node, eval_func):
+        """
+        This function finds the maximum value move for one full turn cycle.
+        It assumes the opponent plays optimally (according to the evaluation
+        function. It returns the maximum value move and the value for the
+        current player.
         
         It takes four arguments:
-        board: the current board space.
-        eval_func: the function which scores a given position.
-        depth: the amount of moves deep the engine searches.
-        deep_search: the function which selects which lines should go beyond the depth.
+        board_node: a hypothetical board state that could be reached..
         
         It returns:
-        A list of tuples of the top five move choices and their associated scores.
-        """        
-        """
-        hyp_board = board.copy()
+        A tuple where the best move is first and the score is second.
+        """    
+        hyp_board = board_node.copy()
+        legal_moves= legal_moves_list(board_node)
         
-        # The moves to be considered
-        candidate_moves = ChessEngine().legal_moves_list(hyp_board)
-        # This is a list that records how many move turns deep each move is
-        move_depth = [0.5]*len(candidate_moves)
-        """
-        """ 
-        This function can analyze arbitrarily deep by using a move stack. With this
-        approach, we keep two lists: a list of moves to analyze and the current
-        analysis depth of each of the aforementioned moves. For each move in the
-        move list, 
-        """
+        scores = []
         
-        """
-        while candidate_moves:
-            if move_depth[-1] == depth:
-        """       
+        # This layer of for loop scores all of the current player's moves - MAX
+        for move in legal_moves:
+            hyp_board.push_san(move)
+            opp_legal_moves = legal_moves_list(hyp_board)
+            
+            # This is a list of the scores after the opponent makes each move
+            response_scores = []
+            
+            # This layer of for loop scores all of the opponent's moves - MIN
+            for opp_move in opp_legal_moves:
+                hyp_board.push_san(opp_move)
+                response_scores.append(eval_func(hyp_board))
+                hyp_board.pop()
+            # We assume the opponent makes the best move, which is the move the minimizes the score
+            scores.append(min(response_scores))
+            hyp_board.pop()
+            
+        # Return the best move and associated maximum score value
+        best_move = (legal_moves[scores.index(max(scores))], max(scores))
+
+        return best_move
     
     def search(self, board, eval_func, depth=4):
         """
@@ -99,50 +135,38 @@ class ChessEngine:
                 best_value = min(best_value, value)
                 hyp_board.pop()
             return best_value
-                
-        
     
-    def minimax(board_node, eval_func):
-        """
-        This function finds the maximum value move for one full turn cycle.
-        It assumes the opponent plays optimally (according to the evaluation
-        function. It returns the maximum value move and the value for the
-        current player.
-        
-        It takes four arguments:
-        board_node: a hypothetical board state that could be reached..
-        
-        It returns:
-        A tuple where the best move is first and the score is second.
-        """    
-        hyp_board = board_node.copy()
-        legal_moves= ChessEngine().legal_moves_list(board_node)
-        
-        scores = []
-        
-        # This layer of for loop scores all of the current player's moves - MAX
-        for move in legal_moves:
-            hyp_board.push_san(move)
-            opp_legal_moves = ChessEngine().legal_moves_list(hyp_board)
-            
-            # This is a list of the scores after the opponent makes each move
-            response_scores = []
-            
-            # This layer of for loop scores all of the opponent's moves - MIN
-            for opp_move in opp_legal_moves:
-                hyp_board.push_san(opp_move)
-                response_scores.append(eval_func(hyp_board))
-                hyp_board.pop()
-            # We assume the opponent makes the best move, which is the move the minimizes the score
-            scores.append(min(response_scores))
-            hyp_board.pop()
-            
-        # Return the best move and associated maximum score value
-        best_move = (legal_moves[scores.index(max(scores))], max(scores))
+    
+class EvalEng:
+    """
+    This class is an abstraction of various specific evaluation functions. 
 
-        return best_move
-                    
-    @staticmethod
+    This class allows users to configure the engine's behavior by supplying
+    different components at initialization time.
+
+    Attributes:
+
+    """  
+    
+    def score_pos(self, board):
+        raise NotImplementedError("Subclasses must implement score_pos")
+    
+   
+class HeuristicEval(EvalEng):
+    """
+    This class attempts to evaluate the position using traditional chess heuristics.
+
+    Traditional heuristics include counting the material, number of squares 
+    controlled, number of pieces developed, pawn structure, king safety, etc.
+
+    Methods:
+        current_material: calculatest the material score of the position.
+        current_space: calculates the number of opponent's squares controlled.
+        current_development: counts the number of pieces developed.
+        current_pawnshain: counts the number of pawn-cahins.
+
+    """  
+    
     def current_material(board):
         """
         This function calculates the current material score. It uses common scoring
@@ -198,7 +222,7 @@ class ChessEngine:
         else:
             opponent_rows = ["1","2","3","4"]
         score = sum(result.count(moves) for moves in opponent_rows)
-        return score
+        return score  
     
     def current_development(board):
         """
@@ -214,9 +238,7 @@ class ChessEngine:
         compared to the opponent.
         """
         hyp_board = board.copy()
-        
-        
-        
+    
     def current_pawnchain(board):
         """
         This function calculates the current number of pawn chains. 
@@ -241,26 +263,25 @@ class ChessEngine:
         # String notation of pawn squares
         pawn_loc = ''.join([chess.square_name(i) for i in pawn_squares_num])
         pawn_loc = re.sub(r'\d+', '', pawn_loc)  # Remove all digits from the string
-        pawn_loc = ''.join(sorted(pawn_loc))     # Sorts the columns alphabetically
+        pawn_loc = ''.join(sorted(pawn_loc))     # Sorts the columns alphabetically 
         
-    
-    def score_pos(self, board, weights=[1, 0.5]):
-        """
-        This function scores a position. It scores based on 4 categories — material,
-        development, squares controlled, king safety, etc.
-        
-        It takes two arguments:
-        board: The current board space.
-        weights: The relative value of different points
-        
-        It returns:
-        A numerical score of the position, where 0 means it is equal, a negative
-        means it favors black, and a positive means it favors white.
-        """
-        w,b = ChessEngine().current_material(board)
-        if board.is_checkmate():
-            score = -np.inf if board.turn else np.inf
-        else:
-            score = weights[0] * (w - b)
-        return score
+        def score_pos(self, board, weights=[1, 0.5]):
+            """
+            This function scores a position. It scores based on 4 categories — material,
+            development, squares controlled, king safety, etc.
+            
+            It takes two arguments:
+            board: The current board space.
+            weights: The relative value of different points
+            
+            It returns:
+            A numerical score of the position, where 0 means it is equal, a negative
+            means it favors black, and a positive means it favors white.
+            """
+            w,b = ChessEngine().current_material(board)
+            if board.is_checkmate():
+                score = -np.inf if board.turn else np.inf
+            else:
+                score = weights[0] * (w - b)
+            return score
     
