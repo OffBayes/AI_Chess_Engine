4501ee3b1fbf94a22e0a325d463d8c563e37aeeb
OffBayes <CWM.DataScience@gmail.com>
2025-08-29 23:30:44 -0400

    Completed ordering logic based on attackers. Removed dependence on UCI strings and replaced with chess.Move. This should improve performance since UCI strings no longer need to be created and destroyed, and the chess.Move is based on bitwise operations.

--- AI_Engine_Parts/Orderer.py
+++ AI_Engine_Parts/Orderer.py
@@ -4,10 +4,10 @@ Created on Sun Jul 27 16:38:12 2025
 
 @author: Prior_Bayes
 """
-
+import chess
 
 class Orderer:
-    def legal_moves_list(self, board):
+    def legal_moves_list(self, board: chess.Board):
         """
         Legal_moves_list takes the moves generator and returns a list of moves.
 
@@ -19,10 +19,9 @@ class Orderer:
         -------
         legal_moves : A list of legal moves.
         """
-        legal_moves = [i.uci() for i in board.legal_moves]
-        return legal_moves
+        return list(board.legal_moves)
 
-    def order_search(self, board):
+    def order_search(self, board: chess.Board) -> list[chess.Move]:
         """
         Determine the order of moves to be searched.
 
@@ -44,7 +43,7 @@ class GreedyOrder(Orderer):
     It inherits from Orderer.
     """
 
-    def order_search(self, board, eval_func):
+    def order_search(self, board: chess.Board, eval_func) -> list[chess.Move]:
         """
         Determine the order of moves to be searched.
 
@@ -65,3 +64,52 @@ class GreedyOrder(Orderer):
         legal_move_scores = [(eval_func(move), move)
                              for move in unsorted_legal_moves]
         return sorted(legal_move_scores, reverse=board.Turn)
+
+
+class AttackOrder(Orderer):
+    """
+    AttackOrder orders the moves by looking at attacking moves first.
+    """
+
+    def get_attackers(self, board: chess.Board) -> chess.SquareSet:
+        """
+        Get the squares with attacking pieces on them.
+
+        Arguments
+        ---------
+        board : the current board state.
+
+        Returns
+        -------
+        attackers: a SquareSet of the attacking pieces
+        """
+        attackers = chess.SquareSet()
+        # Check each enemy piece position
+        for square in chess.SquareSet(board.occupied_co[not board.turn]):
+            # Find which of my pieces are attacking this enemy piece
+            my_attackers = board.attackers(board.turn, square)
+            attackers |= my_attackers
+        return attackers
+
+    def order_search(self, board: chess.Board) -> list[chess.Move]:
+        """
+        Order moves by checking attackers first.
+
+        Arguments
+        ---------
+        board: the current board state.
+
+        Returns
+        -------
+        search_order: a list ordered by the search order.
+        """
+        legal_moves = self.legal_moves_list(board)
+        attackers = self.get_attackers(board)
+
+        # Moves where the starting square is an attacker square get priority
+        capture_moves = [move for move in legal_moves
+                         if move.from_square in attackers]
+        quiet_moves = [move for move in legal_moves
+                       if move.from_square not in attackers]
+
+        return capture_moves + quiet_moves
